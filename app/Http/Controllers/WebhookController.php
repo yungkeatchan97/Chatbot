207f39d6984cf4056157a0ed6df3206f4e2eab31
unknown <narnia_gplus@hotmail.com>
2020-01-05 10:52:03 +0800

    a

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -39,6 +39,8 @@ class WebhookController extends Controller
                 return $this->getCourse();
             case 'getSubject':
                 return $this->getSubject();
+            case 'sendImage':
+                return $this->sendImage();
             default:
                 return $this->defaultFallback();
         }
@@ -69,7 +71,7 @@ class WebhookController extends Controller
         $token = "56935569ff8bdec69eeee97725ea7b37";
         $client = new Client($sid, $token);
         $client->messages->create(
-            "whatsapp:+6011-16408278",
+            "whatsapp:+601116408278",
             array(
                 'from' => "whatsapp:+14155238886",
                 'body' => "Hi Joe! Please find your boarding pass attached. Flight OA2345 departs at 11 pm PST.",
