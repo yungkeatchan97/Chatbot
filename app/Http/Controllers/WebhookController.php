4379af29e801bf31a989dffb3d1a597c7fe473a4
187297 <187297@student.upm.edu.my>
2020-01-06 22:49:46 +0800

    register subject

--- app/Http/Controllers/WebhookController.php
+++ app/Http/Controllers/WebhookController.php
@@ -7,6 +7,7 @@ use App\Handbook;
 use App\Subject;
 use App\Course;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class WebhookController extends Controller
 {
@@ -35,6 +36,8 @@ class WebhookController extends Controller
                 return $this->canGraduate($outputContexts['MatricNumber']);
             case 'RegisterNewSubject.WhatSubject':
                 return $this->whatSubject($outputContexts['MatricNumber']);
+            case 'RegisterNewSubject.Register':
+                return $this->register($outputContexts['MatricNumber'], $parameters['SubjectCode']);
             case 'getCourse':
                 return $this->getCourse();
             case 'getSubject':
@@ -91,6 +94,17 @@ class WebhookController extends Controller
     private function findRequiredLack($matricNumber)
     {
         $student = Student::where('matric_no', '=', $matricNumber)->first();
+        if (is_null($student)){
+            return "I'm sorry but the matric number you provided does not match any record in our database. Please try again.\n
+            You can still ask questions such as:\n
+            1. Course Structure\n
+            2. Registration\n
+            3. Change Programme\n
+            4. Credit Transfer\n
+            5. Late Registration\n
+            6. Registration credit per semester\n
+            7. Academic Adviser";
+        }
         $registereds = $student->registeredSubjects;
         $neededs = $student->handbook->requiredSubjects;
         $lack = array();
@@ -112,6 +126,17 @@ class WebhookController extends Controller
     private function findOptionalLack($matricNumber)
     {
         $student = Student::where('matric_no', '=', $matricNumber)->first();
+        if (is_null($student)){
+            return "I'm sorry but the matric number you provided does not match any record in our database. Please try again.\n
+            You can still ask questions such as:\n
+            1. Course Structure\n
+            2. Registration\n
+            3. Change Programme\n
+            4. Credit Transfer\n
+            5. Late Registration\n
+            6. Registration credit per semester\n
+            7. Academic Adviser";
+        }
         $registereds = $student->registeredSubjects;
         $neededs = $student->handbook->optionalSubjects;
         $lack = array();
@@ -133,6 +158,17 @@ class WebhookController extends Controller
     private function canGraduate($matricNumber)
     {
         $student = Student::where('matric_no', '=', $matricNumber)->first();
+        if (is_null($student)){
+            return "I'm sorry but the matric number you provided does not match any record in our database. Please try again.\n
+            You can still ask questions such as:\n
+            1. Course Structure\n
+            2. Registration\n
+            3. Change Programme\n
+            4. Credit Transfer\n
+            5. Late Registration\n
+            6. Registration credit per semester\n
+            7. Academic Adviser";
+        }
         $lack = $this->findRequiredLack($matricNumber);
         if (empty($lack) && $student->creditHour >= $student->handbook->total_credit_hour){
             $response = "Yes, you can graduate";
@@ -151,6 +187,17 @@ class WebhookController extends Controller
     private function whatSubject($matricNumber)
     {
         $student = Student::where('matric_no', '=', $matricNumber)->first();
+        if (is_null($student)){
+            return "I'm sorry but the matric number you provided does not match any record in our database. Please try again.\n
+            You can still ask questions such as:\n
+            1. Course Structure\n
+            2. Registration\n
+            3. Change Programme\n
+            4. Credit Transfer\n
+            5. Late Registration\n
+            6. Registration credit per semester\n
+            7. Academic Adviser";
+        }
         $lack = $this->findRequiredLack($matricNumber);
         $response = "These are subjects that you MUST take but haven't:";
         foreach ($lack as $item){
@@ -163,6 +210,44 @@ class WebhookController extends Controller
         }
         return $response;
     }
+
+    private function register($matricNumber, $SubjectCode)
+    {
+        $student = Student::where('matric_no', '=', $matricNumber)->first();
+        if (is_null($student)){
+            return "I'm sorry but the matric number you provided does not match any record in our database. Please try again.\n
+            You can still ask questions such as:\n
+            1. Course Structure\n
+            2. Registration\n
+            3. Change Programme\n
+            4. Credit Transfer\n
+            5. Late Registration\n
+            6. Registration credit per semester\n
+            7. Academic Adviser";
+        }
+        $subject = Subject::where('code', '=', $SubjectCode)->first();
+        if (is_null($subject)){
+            return "I'm sorry but the subject code you provided does not match any record in our database. Please try again.";
+        }
+        foreach ($student->registeredSubjects as $registeredSubject){
+            if ($registeredSubject->code == $subject->code && $registeredSubject->result == 4){
+                return "I'm sorry but you have registered this subject and get A";
+            }
+        }
+        if (is_null(DB::transaction(function () use ($subject, $student) {
+            DB::table('registered_subjects')
+                ->insert([
+                    'student_id' => $student->id,
+                    'subject_id' => $subject->id,
+                    'result' => 0
+                ]);
+        }))){
+            return "You have successfully registered ".$SubjectCode;
+        }else{
+            return "Registration failed. Please contact Academic Division for more details.";
+        }
+    }
+
 
     private function getCourse(){
         $course = Course::find(3);
@@ -177,6 +262,5 @@ class WebhookController extends Controller
     private function defaultFallback(){
         return "I can't understand";
     }
-
 
 }
